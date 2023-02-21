21982ecf37dd918b7caa65763d30c7ecc60a2db9
AntiHero <prophreaker@gmail.com>
2023-02-21 15:59:16 +0000

    Deploying to gh-pages from @ AntiHero/spa-deploy-09.2022@10329d5ecaa7c026fb3dc4878a2172f831d2b770 🚀

---
+++ bundle.js
@@ -0,0 +1,1 @@
+!function(){function t(t){const e=window.location.href;document.querySelector("#app").textContent=t?e.includes("#")?e.replace(/#.*/,t):e+t:window.location.href}document.body.addEventListener("click",(e=>{e.target.matches("a")&&t(e.target.hash)})),t()}();
